c3a5b3c1371d1ea79f7f09f16724543acf6f7509
kamalkantmehra2005-max <kamalkantmehra2005@gmail.com>
2026-05-07 22:17:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # ==========================================
 st.set_page_config(
     page_title="Nelumbo",
-    page_icon="🌸",
+    page_icon="🪷",
     layout="wide",
     initial_sidebar_state="expanded"
 )
